b1b175a5e2a3483bc0bdd84e0b498b78acddb17d
Shu000000 <149331560+Shu000000@users.noreply.github.com>
2024-02-11 01:38:52 +0900

    不要なコード削除

--- db/all.js
+++ db/all.js
@@ -2,17 +2,6 @@ exports.getAllRows = function(sql){
     let sqlite3 = require('sqlite3').verbose();
     let db = new sqlite3.Database('todo.db');
 
-    const {Client} = require('pg');
-    const client = new Client({
-        connectionString: process.env.POSTGRES_URL,
-    });
-    client.connect()
-    .then(() => console.log("接続完了"))
-    .then(() => client.query(query))
-    .then(result => console.log(result.rows))
-    .catch((err => console.log(err)))
-    .finally((() => client.end()));
-
     return new Promise((resolve, reject)=>{
         db.all(sql,(err, rows)=>{
             resolve(rows);
